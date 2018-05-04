2a7877dd03621a8b1a7af87a02dc602b4f765a15
Unknown <felix@felixsoum.com>
2018-05-04 10:40:22 -0400

    Add simple player controller and camera

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0, 3, -4);
+    public float rotX = 15;
+    public  float rotSpeedX = 10f;
+    public  float rotSpeedY = 30f;
+
+    GameObject player;
+    float rotY;
+
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(rotX, rotY, 0);
+        transform.position = player.transform.position + Quaternion.Euler(0, rotY, 0) * offset;
+
+        rotX += -Input.GetAxis("Mouse Y") * rotSpeedX * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * rotSpeedX * Time.deltaTime;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float meshLerpSpeed = 0.333f;
+    public float groundDistance = 0.1f;
+    public float extraGravity = 100;
+    public float moveForce = 3;
+    public GameObject mesh;
+    new Rigidbody rigidbody;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyMovementInput();
+        ApplyFakeGravity();
+    }
+
+    void ApplyMovementInput()
+    {
+        Vector3 cameraRight = Camera.main.transform.right;
+        cameraRight.y = 0;
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0;
+
+        Vector3 move = cameraRight.normalized * Input.GetAxis("Horizontal") + cameraForward.normalized * Input.GetAxis("Vertical");
+        rigidbody.AddForce(move * moveForce, ForceMode.VelocityChange);
+    }
+
+    void ApplyFakeGravity()
+    {
+        if (!IsGrounded())
+        {
+            rigidbody.AddForce(Vector3.down * extraGravity, ForceMode.Acceleration);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Vector3 groundOrigin = transform.position + Vector3.up * groundDistance;
+        Vector3 ray = Vector3.down * groundDistance * 2;
+        var hits = Physics.RaycastAll(groundOrigin, Vector3.down, groundDistance * 2);
+        Debug.DrawRay(groundOrigin, ray, Color.red);
+        foreach (var hit in hits)
+        {
+            if (hit.collider.tag == "Player")
+            {
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
 }
