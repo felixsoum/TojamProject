8bc69946cc2019507dcf4985893c5a12da790848
Unknown <felix@felixsoum.com>
2018-05-06 20:47:37 -0400

    Add projection screen

---
+++ Assets/Scripts/ProjectorScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ProjectorScript : MonoBehaviour
+{
+    public Material[] materials;
+    public float timePerSlide = 3;
+    public MeshRenderer meshRenderer;
+
+    int currentIndex;
+
+    void Awake()
+    {
+        InvokeRepeating("NextSlide", timePerSlide, timePerSlide);
+    }
+
+    void NextSlide()
+    {
+        currentIndex++;
+        currentIndex %= materials.Length;
+        meshRenderer.material = materials[currentIndex];
+    }
+}
